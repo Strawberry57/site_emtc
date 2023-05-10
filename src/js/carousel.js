6d726f76b347e92c50908a7d8801dfe67dd89cb4
Đinh Sỹ Dũng <dungbn2000@gmail.com>
2023-05-10 15:58:21 +0700

    fix home page

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -8,6 +8,7 @@ const fill_left_pro = document.querySelector(".fill_left_pro_carousel");
 const fill_right_pro = document.querySelector(".fill_right_pro_carousel");
 const arrowLeft_product = document.querySelector(".left-carousel-product");
 const arrowRight_product = document.querySelector(".right-carousel-product");
+const listLI = document.querySelectorAll(".list_li");
 const box_product = document.querySelectorAll(".box-product");
 const listImage = [
   {
@@ -43,6 +44,21 @@ let countValue = 0;
 arrowLeft.addEventListener("click", () => {
   position = position - 1;
   count = position;
+  checkColorArrowD();
+  if (count > -1) {
+    selectFnc(count);
+  }
+});
+
+arrowRight.addEventListener("click", () => {
+  position = position + 1;
+  count = position;
+  if (count < listLI.length) {
+    selectFnc(count);
+  }
+  checkColorArrowI();
+});
+function checkColorArrowD() {
   if (count < 0) {
     position = 0;
     count = 0;
@@ -51,45 +67,43 @@ arrowLeft.addEventListener("click", () => {
     if (count === 0) {
       fill_left.style.fill = "#A5A8AA";
       arrowLeft.style.cursor = "default";
-      selectFnc(count);
+      // selectFnc(count);
     } else {
       fill_right.style.fill = "#2B5AA9";
       arrowLeft.style.cursor = "pointer";
 
-      imageShow.src = listImage[count].src;
-      textShow.innerHTML = listImage[count].text;
+      // imageShow.src = listImage[count].src;
+      // textShow.innerHTML = listImage[count].text;
       position = count;
     }
-    selectFnc(count);
+    // selectFnc(count);
   }
-});
-
-arrowRight.addEventListener("click", () => {
-  position = position + 1;
-  count = position;
-  if (count > listImage.length - 1) {
-    position = listImage.length - 1;
+}
+function checkColorArrowI() {
+  if (count > listLI.length - 1) {
+    position = listLI.length - 1;
     arrowLeft.style.cursor = "default";
     return;
   } else {
-    if (count === listImage.length - 1) {
+    if (count === listLI.length - 1) {
       fill_right.style.fill = "#A5A8AA";
       arrowRight.style.cursor = "default";
       arrowLeft.style.cursor = "pointer";
-      selectFnc(count);
+      // selectFnc(count);
     }
     fill_left.style.fill = "#2B5AA9";
     arrowLeft.style.cursor = "pointer";
-    imageShow.src = listImage[count].src;
-    textShow.innerHTML = listImage[count].text;
+    // imageShow.src = listImage[count].src;
+    // textShow.innerHTML = listImage[count].text;
     position = count;
-    if (count < listImage.length - 1) {
-      selectFnc(count);
-    }
+    // if (count < listImage.length - 1) {
+    //   selectFnc(count);
+    // }
   }
-});
+}
 
 function selectFnc(e) {
+  count = e;
   const li_selected = document.getElementById(`${e}`);
   const li_selected_before = document.querySelector(".selected_li");
   if (li_selected.id === li_selected_before.id) {
@@ -97,14 +111,20 @@ function selectFnc(e) {
   } else {
     li_selected.classList.add("selected_li");
     li_selected_before.classList.remove("selected_li");
-    const number = Math.floor(Math.random() * 3);
-
-    imageShow.src = listImage[number].src;
-    textShow.innerHTML = listImage[number].text;
+    const serviceBoxShow = document.querySelector(".show");
+    serviceBoxShow.classList.remove("show");
+    const serviceBox = document.querySelector(`.service-box-${e}`);
+    serviceBox.classList.add("show");
+    // imageShow.src = listImage[number].src;
+    // textShow.innerHTML = listImage[number].text;
   }
   const back = document.querySelector(".list-background");
-  console.log(e);
   back.innerHTML = `0` + (Number(e) + 1);
+  if (count > position) {
+    checkColorArrowI();
+  } else {
+    checkColorArrowD();
+  }
 }
 
 arrowRight_product.addEventListener("click", () => {
